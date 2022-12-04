bd85ab89d3010485ec39720e53dab49a065db67b
MdRaihanAli <101347732+MdRaihanAli@users.noreply.github.com>
2022-12-04 16:44:01 +0400

    Add files via upload

---
+++ four_26_to_30/script.js
@@ -0,0 +1,168 @@
+// // Create a arry
+// let names = ["raihan", "mohammod","roni","shadhin","esha","akash"];
+// let name1 = ["rohim","korim","majed"]
+// let raju = "raihan";
+// console.log(names);
+// console.log(name1[1]);
+// console.log(name1.length);
+
+// names.push("raju");
+// console.log(names);
+
+// names.pop();
+// console.log(names);
+
+// names.shift()
+// console.log(names);
+
+// names.unshift("raju","manik")
+// console.log(names);
+
+// names.splice(1,2,"okey");
+// console.log(names);
+
+// let name = names.slice(1,3);
+// console.log(name);
+
+// let nam = names.sort();
+// nam.reverse()
+// console.log(nam);
+
+
+// // Arry with loop
+// for(let i =0; i<name1.length; i++){
+//     names.push(name1[i]) 
+// };
+
+// for(let i =0; i<names.length; i++){
+//     let sName = names[i];
+//     console.log(sName);
+// }
+
+
+// // task 8
+// let number = [10,65,92,35,64,45,12,32,25,42,]
+// function highestScore(e) {
+//     let max = 0;
+//     for(let i=0; i<e.length; i++ ){
+//         if(max<e[i]){
+//             max = e[i]
+//         }
+//     }
+//     console.log(max);
+// }
+// highestScore(number);
+
+
+// Object
+// let friend1 = {
+//     name : "manik",
+//     age : 25,
+//     phone : 0172,
+// }
+
+// console.log(friend1);
+// console.log(friend1.phone);
+
+// function Friend(name,age,phone){
+//     this.name = name;
+//     this.age = age;
+//     this.phone = phone;
+
+//     this.Display = function(e){
+//         console.log(name);
+//         console.log(phone);
+//     }
+// }
+// let manik = new Friend("manki",25,017674);
+// let saju = new Friend("saju",20,017674);
+// manik.Display();
+// saju.Display();
+
+
+// 2D Array
+// let plears = [ 
+//     ["sakib", 17],
+//     ["masrafi", 70],
+//     ["tamim", 30],
+//     ["momin", 40],
+// ]
+// let maxer = plears[0][1];
+// let count = []
+// for(let i = 0; i<plears.length; i++){
+//     if(maxer<plears[i][1]){
+//         maxer = plears[i][0];  
+//     }
+    
+// }
+// console.log(maxer);
+
+
+
+
+// task 
+// create a 3D Array
+// let name = [];
+// let div = [];
+// let num = [];
+// let plears = [
+//     ["Sakib","dhaka",45],
+//     ["Mahmudullah","khulna",32],
+//     ["Musfuk","rajshahi",66],
+//     ["Miraj","shylet",55],
+// ]
+// for (let i = 0; i < plears.length; i++) {
+//    let  namep = plears[i][0];
+//    name.push(namep);
+
+//    let  divP = plears[i][1];
+//    div.push(divP);
+
+//    let  numP = plears[i][2];
+//    num.push(numP);
+// }
+
+// console.log(name);
+// console.log(div);
+// console.log(num);
+
+
+// task
+// let Fizz = 0;
+// let Buzz = 0;
+// let FizzBuzz = 0;
+
+
+// for (let i = 0; i <= 100; i++) {
+//     if(i%3==0){
+//         console.log("Fizz");
+//         Fizz++;
+//     }
+//     if(i%5 == 0){
+//         console.log("Buzz");
+//         Buzz++;
+//     }
+//     if(i%3==0 && i%5==0){
+//         console.log("FizzBuzz");
+//         FizzBuzz++;
+//     }
+
+   
+// }
+// console.log(Fizz);
+// console.log(Buzz);
+// console.log(FizzBuzz);
+
+
+
+
+
+
+
+
+
+
+
+// let name = ["Sakib","Mahmudullah","Musfik","Miraj"]
+// let div = ["dhaka","khulna","rajshahi","shylet"]
+// let num = [45,32,66,55]
